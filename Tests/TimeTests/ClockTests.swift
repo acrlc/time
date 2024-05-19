b86016be8d596e0d9428efa95bbe4decbaa0e3fb
William Luke <williamluke@me.com>
2024-05-19 00:06:38 -0500

    update tests

--- Tests/TimeTests/ClockTests.swift
+++ Tests/TimeTests/ClockTests.swift
@@ -2,9 +2,32 @@
 import XCTest
 
 final class ClockTests: XCTestCase {
- func testClock() async throws {
-  let tickClock = ContinuousTickClock()
-  try await tickClock.sleep(until: .now.advanced(by: .init(5)))
-  print("Hello")
+ var timer: some TimerProtocol = .standard
+
+ override func setUp() {
+  print("Starting timer ...")
+  timer.fire()
+ }
+
+ func testDateClock() async throws {
+  let clock = DateClock()
+  
+  try await clock.sleep(until: .now + 0.75)
+  let time = timer.elapsed
+
+  XCTAssertEqual(time, 0.75, accuracy: 0.01)
+  dump(clock)
+  print(time)
+ }
+
+ func testTimeClock() async throws {
+  let clock = TimeClock()
+  
+  try await clock.sleep(until: .now + 1.25)
+  let time = timer.elapsed
+  
+  XCTAssertEqual(time, 1.25, accuracy: 0.01)
+  dump(clock)
+  print(time)
  }
 }
--- Tests/TimeTests/TimeTests.swift
+++ Tests/TimeTests/TimeTests.swift
@@ -2,9 +2,15 @@
 import XCTest
 
 final class TimeTests: XCTestCase {
+ var timer: some TimerProtocol = .standard
+
+ override func setUp() {
+  print("Resetting timer ...")
+  timer.reset()
+ }
+
  func testTimer() async throws {
   let times: [UInt32] = [1, 2]
-  var timer: TimerProtocol = .standard
 
   for int in times {
    XCTAssertEqual(timer.fireDate, .distantFuture)
@@ -13,11 +19,10 @@ final class TimeTests: XCTestCase {
    try await Task.sleep(for: .seconds(int))
 
    // read elapsed time
-   let elapsed = timer.elapsed
-   let time = Time(elapsed.seconds.rounded(.down))
+   let time = timer.elapsed
    let projectedTime = try XCTUnwrap(Time("\(int)s"))
 
-   XCTAssertEqual(time.description, projectedTime.description)
+   XCTAssertEqual(time, projectedTime, accuracy: 0.1)
 
    timer.invalidate() // invalidates the timer, if needed
    XCTAssertEqual(timer.fireDate, .distantPast)
@@ -25,31 +30,31 @@ final class TimeTests: XCTestCase {
    timer.reset() // resets the timer, when needed to skip measurements
    XCTAssertEqual(timer.fireDate, .distantFuture)
   }
+
+  dump(timer)
+  XCTAssertEqual(timer.elapsed, .nanosecond)
  }
 
  func testSize() async throws {
   let size: Size = 3
   let times: [UInt32] = (0 ..< 3).map { _ in 333_333 }
-  var timer: TimerProtocol = .standard
 
   timer.fire()
 
   // iterate 3 times, sleep for 333333µs three times
   for _ in size {
    for int in times {
-    try await Task.sleep(for: .milliseconds(int / 1000))
+    try await Task.sleep(for: .microseconds(int))
    }
   }
 
-  let elapsed = timer.elapsed
-  let time = Time(elapsed.seconds.rounded(.down))
+  let time = timer.elapsed
   let projectedTime = try XCTUnwrap(Time("3s"))
 
-  XCTAssertEqual(time.description, projectedTime.description)
+  XCTAssertEqual(time, projectedTime, accuracy: 0.2)
 
-  let average = elapsed.amortized(over: size)
-  let averageRounded = Time(average.seconds.rounded(.down))
-
-  XCTAssertEqual(averageRounded, Time(1))
+  let average = time.amortized(over: size)
+  XCTAssertEqual(average, 1, accuracy: 0.1)
+  dump(average)
  }
 }
