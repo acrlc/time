a8e3ef8118cc2dcf86f3b708417ce60a7667d707
William Luke <williamluke@me.com>
2024-05-19 00:06:22 -0500

    consolidate changes to timeclock - dateclock - and tick

--- Sources/Tick.swift
+++ Sources/Tick.swift
@@ -1,11 +1,11 @@
-// https://www.github.com/swift-collections-benchmark
 #if !USE_FOUNDATION_DATE && (os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
-import Foundation // For the side effect of reexporting Darwin/Glibc
+import Foundation
 
-public struct Tick: Equatable, DateProtocol {
- let _value: timespec
+/// A point in time based on a reference and used to measure relative events.
+public struct Tick: Equatable, Hashable, DateProtocol {
+ private let _value: timespec
 
- init(_value: timespec) {
+ fileprivate init(_value: timespec) {
   self._value = _value
  }
 
@@ -31,32 +31,65 @@ public struct Tick: Equatable, DateProtocol {
   )
  }
 
- public func elapsedTime(since start: Tick) -> Time {
-  let s = Double(_value.tv_sec - start._value.tv_sec)
-  let ns = Double(_value.tv_nsec - start._value.tv_nsec)
-  return Time(s + ns / 1e9)
- }
-
- public var timeIntervalSinceNow: Time { elapsedTime(since: .now) }
-
  public static let distantPast =
   Tick(_value: timespec(tv_sec: -.max, tv_nsec: -.max))
  public static let distantFuture =
   Tick(_value: timespec(tv_sec: .max, tv_nsec: .max))
+
+ public func elapsedTime(since start: Tick) -> Time {
+  let s = Double(_value.tv_sec - start._value.tv_sec)
+  let ns = Double(_value.tv_nsec - start._value.tv_nsec)
+
+  return Time(s + ns / 1e9)
+ }
+
+ public func advanced(by duration: Time) -> Tick {
+  let seconds = duration.seconds
+  let rounded = seconds.rounded(.down)
+  let remainder = seconds.remainder(dividingBy: rounded)
+  return Tick(
+   _value: timespec(
+    tv_sec: _value.tv_sec + (rounded > 0 ? Int(rounded) : 0),
+    tv_nsec: _value.tv_nsec + (remainder > 0 ? Int(remainder * 1e9) : 0)
+   )
+  )
+ }
+
+ public func duration(to other: Tick) -> Time {
+  let s = Double(other._value.tv_sec - _value.tv_sec)
+  let ns = Double(other._value.tv_nsec - _value.tv_nsec)
+  return Time(s + ns / 1e9)
+ }
+
  public static func == (lhs: Tick, rhs: Tick) -> Bool {
   lhs._value.tv_sec == rhs._value.tv_sec &&
    lhs._value.tv_nsec == rhs._value.tv_nsec
  }
+
+ public static func < (lhs: Tick, rhs: Tick) -> Bool {
+  lhs._value.tv_sec <= rhs._value.tv_sec &&
+   lhs._value.tv_nsec <= rhs._value.tv_nsec
+ }
+}
+
+extension timespec: Hashable {
+ public static func == (lhs: timespec, rhs: timespec) -> Bool {
+  lhs.tv_sec == rhs.tv_sec && lhs.tv_nsec == rhs.tv_nsec
+ }
+
+ public func hash(into hasher: inout Hasher) {
+  hasher.combine(tv_sec)
+  hasher.combine(tv_nsec)
+ }
 }
 
 #else
+import struct Foundation.Date
 
-import Foundation
+public struct Tick: Equatable, Hashable, DateProtocol {
+ private let _value: Date
 
-public struct Tick: Equatable, DateProtocol {
- let _value: Date
-
- init(_value: Date) {
+ fileprivate init(_value: Date) {
   self._value = _value
  }
 
@@ -64,20 +97,43 @@ public struct Tick: Equatable, DateProtocol {
   Tick(_value: Date())
  }
 
- public func elapsedTime(since start: Tick) -> Time {
-  Time(Double(_value.timeIntervalSince(start._value)))
- }
- 
- public var timeIntervalSinceNow: Time { elapsedTime(since: .now) }
-
  public static var resolution: Time {
   .nanosecond
  }
 
  public static let distantPast = Tick(_value: .distantPast)
  public static let distantFuture = Tick(_value: .distantFuture)
+
+ public func elapsedTime(since start: Tick) -> Time {
+  Time(_value.timeIntervalSince(start._value))
+ }
+
+ public func advanced(by duration: Time) -> Tick {
+  Tick(_value: _value.advanced(by: duration.rawValue))
+ }
+
+ public func duration(to other: Tick) -> Time {
+  Time(_value.duration(to: other._value))
+ }
+
  public static func == (lhs: Tick, rhs: Tick) -> Bool {
   lhs._value == rhs._value
  }
+
+ public static func < (lhs: Tick, rhs: Tick) -> Bool {
+  lhs._value < rhs._value
+ }
 }
 #endif
+
+public extension Tick {
+ var timeIntervalSinceNow: Time { elapsedTime(since: .now) }
+
+ static func + (lhs: Tick, rhs: Time) -> Tick {
+  lhs.advanced(by: rhs)
+ }
+
+ static func += (lhs: inout Tick, rhs: Time) {
+  lhs = lhs.advanced(by: rhs)
+ }
+}
--- Sources/TimeClock.swift
+++ Sources/TimeClock.swift
@@ -1,45 +1,36 @@
-import Foundation // For the side effect of reexporting Darwin/Glibc
 import struct Foundation.Date
 import struct Foundation.TimeInterval
 
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 /// A clock for events that rely on date
 public struct DateClock: Clock {
  @inline(__always)
  public var now: Date { .now }
- public var minimumResolution: Date.TimeInterval = 1e-9
- private let clock = ContinuousClock()
+ public var minimumResolution: TimeInterval = 1e-9
  public init() {}
- public init(minimumResolution: Date.TimeInterval) {
+ public init(minimumResolution: TimeInterval) {
   self.minimumResolution = minimumResolution
  }
 }
 
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public extension DateClock {
  func sleep(until deadline: Date, tolerance: TimeInterval? = nil) async throws {
-  let duration: Swift.Duration =
-   .seconds(deadline.timeIntervalSince(now))
-  try await clock.sleep(
-   until: .now.advanced(by: duration),
-   tolerance: .seconds(tolerance ?? minimumResolution)
-  )
- }
-
- func sleep(tolerance: TimeInterval? = nil) async throws {
-  try await clock.sleep(
-   until: .now.advanced(by: Swift.Duration.seconds(now.timeIntervalSinceNow)),
-   tolerance: .seconds(tolerance ?? minimumResolution)
+  try await Task.sleep(
+   until: .now.advanced(by: .seconds(now.duration(to: deadline))),
+   tolerance: .seconds(tolerance ?? minimumResolution), clock: .continuous
   )
  }
 }
 
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public extension Clock where Self == DateClock {
  static var date: Self { Self() }
+ static func date(minimumResolution: TimeInterval) -> Self {
+  Self(minimumResolution: minimumResolution)
+ }
 }
 
-@available(macOS 13.0, *)
 extension TimeInterval: DurationProtocol {
  public static func * (lhs: Double, rhs: Int) -> Double {
   lhs * Double(rhs)
@@ -48,136 +39,58 @@ extension TimeInterval: DurationProtocol {
  public static func / (lhs: Double, rhs: Int) -> Double {
   lhs / Double(rhs)
  }
-
- public var duration: Duration { .seconds(self) }
 }
 
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+public extension TimeInterval {
+ @inline(__always)
+ var duration: Duration { .seconds(self) }
+}
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 extension Date: InstantProtocol {
  public func duration(to other: Self) -> TimeInterval {
-  timeIntervalSince(other)
+  Double(other.timeIntervalSince(self))
  }
 }
 
 // MARK: - Tick Implementation
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 /// A clock for events that rely on relative time
 public struct TimeClock: Clock {
  @inline(__always)
  public var now: Tick { .now }
- public var minimumResolution: Time = .nanosecond
- private let clock = ContinuousClock()
+ public var minimumResolution: Time = Tick.resolution
  public init() {}
  public init(minimumResolution: Time) {
   self.minimumResolution = minimumResolution
  }
 }
 
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public extension TimeClock {
  func sleep(until deadline: Tick, tolerance: Time? = nil) async throws {
-  let duration: Swift.Duration = deadline.elapsedTime(since: now).duration
-  try await clock.sleep(
-   until: .now.advanced(by: duration),
-   tolerance: .seconds((tolerance ?? minimumResolution).rawValue)
-  )
- }
-
- func sleep(tolerance: Time? = nil) async throws {
-  try await clock.sleep(
-   until: .now.advanced(
-    by: Swift.Duration.seconds(now.timeIntervalSinceNow.rawValue)
-   ),
-   tolerance: .seconds((tolerance ?? minimumResolution).rawValue)
+  try await Task.sleep(
+   until: .now.advanced(by: .seconds(now.duration(to: deadline).rawValue)),
+   tolerance: .seconds((tolerance ?? minimumResolution).rawValue),
+   clock: .continuous
   )
  }
 }
 
-@available(macOS 13.0, *)
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 public extension Clock where Self == TimeClock {
  static var time: Self { Self() }
+ static func date(minimumResolution: Time) -> Self {
+  Self(minimumResolution: minimumResolution)
+ }
 }
 
 @available(macOS 13.0, *)
-extension Time: DurationProtocol, AdditiveArithmetic {
- public static func + (lhs: Time, rhs: Time) -> Time {
-  Time(lhs.rawValue + rhs.rawValue)
- }
-
- public static func - (lhs: Time, rhs: Time) -> Time {
-  Time(lhs.rawValue - rhs.rawValue)
- }
-
- public static func / (lhs: Time, rhs: Time) -> Time {
-  Time(lhs.rawValue / rhs.rawValue)
- }
-
- public static func * (lhs: Time, rhs: Int) -> Time {
-  Time(lhs.rawValue * Double(rhs))
- }
-
- public static func / (lhs: Time, rhs: Int) -> Time {
-  Time(lhs.rawValue / Double(rhs))
- }
-
- public static func / (lhs: Time, rhs: Time) -> Double {
-  lhs.rawValue / rhs.rawValue
- }
-
+extension Time: DurationProtocol {
+ @inline(__always)
  public var duration: Duration { .seconds(rawValue) }
 }
 
 @available(macOS 13.0, *)
-extension Tick: InstantProtocol, Hashable {
- #if !USE_FOUNDATION_DATE && (os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
- public func advanced(by duration: Time) -> Tick {
-  let start = Tick(
-   _value: .init(duration.duration)
-  )
-  let s = _value.tv_sec + start._value.tv_sec
-  let ns = _value.tv_nsec + start._value.tv_nsec
-  return Tick(_value: timespec(tv_sec: s, tv_nsec: ns))
- }
-
- public static func < (lhs: Tick, rhs: Tick) -> Bool {
-  lhs._value.tv_sec <= rhs._value.tv_sec &&
-   lhs._value.tv_nsec <= rhs._value.tv_nsec
- }
-
- public func duration(to other: Self) -> Time {
-  let s = Double(other._value.tv_sec - _value.tv_sec)
-  let ns = Double(other._value.tv_nsec - _value.tv_nsec)
-  return Time(s + ns / 1e9)
- }
- #else
- public func advanced(by duration: Time) -> Tick {
-  Tick(_value: _value.advanced(by: duration.rawValue))
- }
-
- public static func < (lhs: Tick, rhs: Tick) -> Bool {
-  lhs._value < rhs._value
- }
-
- public func duration(to other: Self) -> Time {
-  Time(_value.timeIntervalSince(other._value))
- }
- #endif
-
- public func hash(into hasher: inout Hasher) {
-  hasher.combine(_value)
- }
-}
-
-#if !USE_FOUNDATION_DATE && (os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
-extension timespec: Hashable {
- public static func == (lhs: timespec, rhs: timespec) -> Bool {
-  lhs.tv_sec == rhs.tv_sec &&
-   lhs.tv_nsec == rhs.tv_nsec
- }
-
- public func hash(into hasher: inout Hasher) {
-  hasher.combine(tv_sec)
-  hasher.combine(tv_nsec)
- }
-}
-#endif
+extension Tick: InstantProtocol {}
